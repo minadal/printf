eb5cc48bb1966f2157bf59c011acee418e77a16c
ED LAURENT NYAKIEYA <eddylaurent1029@gmail.com>
2023-07-26 04:14:34 +0300

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -5,16 +5,14 @@
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
- *
- * Return: Precision.
+  * Return: Precision.
  */
 int get_precision(const char *format, int *i, va_list list)
 {
-	int curr_i = *i + 1;
-	int precision = -1;
-
-	if (format[curr_i] != '.')
-		return (precision);
+int curr_i = *i + 1;
+int precision = -1;
+if (format[curr_i] != '.')
+return (precision);
 
 	precision = 0;
 
