28bbff925f234c7385477993d312f1cdaf4efacd
ED LAURENT NYAKIEYA <eddylaurent1029@gmail.com>
2023-07-26 04:01:13 +0300

    Update get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * get_flags - Calculates active flags
  * @format: Formatted string in which to print the arguments
@@ -17,12 +16,12 @@ int get_flags(const char *format, int *i)
 
 	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
-		for (j = 0; FLAGS_CH[j] != '\0'; j++)
-			if (format[curr_i] == FLAGS_CH[j])
-			{
-				flags |= FLAGS_ARR[j];
-				break;
-			}
+	for (j = 0; FLAGS_CH[j] != '\0'; j++)
+	if (format[curr_i] == FLAGS_CH[j])
+	{
+	flags |= FLAGS_ARR[j];
+	break;
+	}
 
 		if (FLAGS_CH[j] == 0)
 			break;
